1450ea1ec2216f09304c98cfdc9fdcc9e4173dfa
someyakoki <yukiteru274@icloud.com>
2020-05-22 04:03:01 +0900

    投稿機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,23 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::with(['user'])->get();
 
-    return view('index', ['posts' => $posts]);
+        return view('index', ['posts' => $posts]);
     }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+{
+    $post = new Post;
+    $post->fill($request->all());
+    $post->user()->associate(Auth::user()); // ★
+    $post->save();
+
+    return redirect()->to('/'); // '/' へリダイレクト
+}
 }
